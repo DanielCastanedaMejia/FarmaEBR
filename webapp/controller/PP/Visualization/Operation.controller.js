286eb62b44bee39ea2e42bcad9140cb17ee0f52e
axelmz <axel.melendez@alumnos.udg.mx>
2021-11-26 17:44:04 -0600

    Archivo de modelo de la cabecera del producto agregado

--- webapp/controller/PP/Visualization/Operation.controller.js
+++ webapp/controller/PP/Visualization/Operation.controller.js
@@ -31,8 +31,8 @@ sap.ui.define([
             };
             this.byId("PMComponentList").setModel(this.getOwnerComponent().getModel("componentsModel"));
             //this._base_onloadTable2("PMComponentList", aData, "FARMA/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/components_raiz", "Componentes", "");
-            this._base_onloadTable("PMMAFList", aData, "GIM/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/maf_operation_BD", "MAF", "");
-            this._base_onloadHeader(aData, "FARMA/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/operation_header", "Cabecera");
+            //this._base_onloadTable("PMMAFList", aData, "GIM/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/maf_operation_BD", "MAF", "");
+            //this._base_onloadHeader(aData, "FARMA/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/operation_header", "Cabecera");
 
             var columns = {
                 columns: [
