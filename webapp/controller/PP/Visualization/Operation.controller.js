9a58c7f7becd825c02adbff51cf49e4e6ae454ef
Daniel Castaneda <dcastaneda@baycoconsulting.com>
2021-11-26 17:41:06 -0600

    added maf model to maf table

--- webapp/controller/PP/Visualization/Operation.controller.js
+++ webapp/controller/PP/Visualization/Operation.controller.js
@@ -31,7 +31,8 @@ sap.ui.define([
             };
             this.byId("PMComponentList").setModel(this.getOwnerComponent().getModel("componentsModel"));
             //this._base_onloadTable2("PMComponentList", aData, "FARMA/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/components_raiz", "Componentes", "");
-            this._base_onloadTable("PMMAFList", aData, "GIM/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/maf_operation_BD", "MAF", "");
+            this.byId("PMMAFList").setModel(this.getOwnerComponent().getModel("mafModel"));
+            //this._base_onloadTable("PMMAFList", aData, "GIM/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/maf_operation_BD", "MAF", "");
             this._base_onloadHeader(aData, "FARMA/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/operation_header", "Cabecera");
 
             var columns = {
