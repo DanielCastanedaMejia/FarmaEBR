3371694ea0fca3c920b7b8547ea447234395afae
Daniel Castaneda <dcastaneda@baycoconsulting.com>
2021-11-28 19:51:11 -0600

    fix: coherent quantities in operation header

--- webapp/controller/PP/Visualization/Operation.controller.js
+++ webapp/controller/PP/Visualization/Operation.controller.js
@@ -29,17 +29,40 @@ sap.ui.define([
                 "NUM_ORDEN": oArgs.orden,
                 "OPERACION": oArgs.operacion
             };
+            
             this.byId("PMComponentList").setModel(this.getOwnerComponent().getModel("componentsModel"));
             //this._base_onloadTable2("PMComponentList", aData, "FARMA/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/components_raiz", "Componentes", "");
             this.byId("PMMAFList").setModel(this.getOwnerComponent().getModel("mafModel"));
             //this._base_onloadTable("PMMAFList", aData, "GIM/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/maf_operation_BD", "MAF", "");
-            var sProp = this.getOwnerComponent().getModel("masterModel").getProperty("/selectedOrder");
-            sProp = this.getOwnerComponent().getModel("ordersModel").getProperty(sProp + "/NUM_ORDEN");
-            this.getOwnerComponent().getModel("headModel").setProperty("/ORDEN", sProp);
-            sProp = this.getOwnerComponent().getModel("fasesModel").getProperty(sProp + "/Ope");
-            this.getOwnerComponent().getModel("headModel").setProperty("/OPERACION", sProp);
 
-            var newModel = new JSONModel(this.getOwnerComponent().getModel("headModel").getProperty("/"));
+            var oMasterModel = this.getOwnerComponent().getModel("masterModel"),
+                oHeadModel = this.getOwnerComponent().getModel("headModel"),
+                oOrderModel = this.getOwnerComponent().getModel("ordersModel"),
+                oOpeModel = this.getOwnerComponent().getModel("fasesModel"),
+                sOrderPath = oMasterModel.getProperty("/selectedOrder"),
+                sOpePath = oMasterModel.getProperty("/selectedFase"),
+                fDone = oOpeModel.getProperty(sOpePath + "/avance"),
+                iDone, iLeft;
+
+            const sOrder = oOrderModel.getProperty(sOrderPath + "/NUM_ORDEN"),
+                iQty = oOrderModel.getProperty(sOrderPath + "/CANTIDAD_PROGRAMADA"),
+                sOpe = oOpeModel.getProperty(sOpePath + "/Ope");
+
+            if(fDone == "NA") {
+                iDone = 0;
+            } else {
+                iDone = Math.floor(iQty * (fDone / 100));
+            }
+            iLeft = iQty - iDone;
+
+            oHeadModel.setProperty("/FALTANTE", iLeft);
+            oHeadModel.setProperty("/PRODUCIDO", iDone);
+            oHeadModel.setProperty("/ORDEN", sOrder);
+            oHeadModel.setProperty("/OPERACION", sOpe);
+            oHeadModel.setProperty("/AVANCE", fDone);
+            oHeadModel.setProperty("/PROGRAMADO", iQty);
+
+            var newModel = new JSONModel(oHeadModel.getProperty("/"));
 
             this.getView().setModel(newModel);
             //this._base_onloadHeader(aData, "FARMA/DatosTransaccionales/Produccion/Ordenes/Visualizar/Transaction/operation_header", "Cabecera");
