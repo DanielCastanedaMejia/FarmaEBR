bd73fa2e3aab201263350beb46c37e3b110e20ff
axelmz <axel.melendez@alumnos.udg.mx>
2021-12-21 14:15:43 -0600

    Agregar Spots funcional

--- webapp/controller/GeoSpotEdit.controller.js
+++ webapp/controller/GeoSpotEdit.controller.js
@@ -1,20 +1,62 @@
 sap.ui.define([
     "sap/ui/demo/webapp/controller/BaseController",
-    "sap/ui/core/routing/History"
+    "sap/ui/core/routing/History",
+    // @ts-ignore
+// @ts-ignore
 ], function (BaseController, History) {
     "use strict";
 
     return BaseController.extend("sap.ui.demo.webapp.controller.GeoSpotEdit", {
         onInit: function () {
             this.aPath = "";
+            // @ts-ignore
             var oRouter = this.getRouter();
+            // @ts-ignore
             oRouter.getRoute("GeoSpotEdit").attachMatched(this._onRouteMatched, this);
         },
+        // @ts-ignore
+        // @ts-ignore
         _onRouteMatched: function (oEvent) {
-            //console.log("Editar spots");
             this.getView().setModel(this.getOwnerComponent().getModel("spots"));
-            //console.log(this.getView().byId("spotsGeo"));
-            //console.log(this.getView().getModel();
+        },
+        fillTypeComboBox: function () {
+            var oThis = this;
+
+            var itemTest = new sap.ui.core.Item();
+            itemTest.setText("Success");
+            itemTest.setKey("0");
+            // @ts-ignore
+            oThis.byId("comboTypes").addItem(itemTest);
+
+            itemTest = new sap.ui.core.Item();
+            itemTest.setText("Default");
+            itemTest.setKey("1");
+            // @ts-ignore
+            oThis.byId("comboTypes").addItem(itemTest);
+
+            itemTest = new sap.ui.core.Item();
+            itemTest.setText("Warning");
+            itemTest.setKey("2");
+            // @ts-ignore
+            oThis.byId("comboTypes").addItem(itemTest);
+
+            itemTest = new sap.ui.core.Item();
+            itemTest.setText("None");
+            itemTest.setKey("3");
+            // @ts-ignore
+            oThis.byId("comboTypes").addItem(itemTest);
+
+            itemTest = new sap.ui.core.Item();
+            itemTest.setText("Inactive");
+            itemTest.setKey("4");
+            // @ts-ignore
+            oThis.byId("comboTypes").addItem(itemTest);
+
+            itemTest = new sap.ui.core.Item();
+            itemTest.setText("Error");
+            itemTest.setKey("5");
+            // @ts-ignore
+            oThis.byId("comboTypes").addItem(itemTest);
         },
         onSpotRowClicked: function (oEvent) {
             var oThis = this;
@@ -23,19 +65,42 @@ sap.ui.define([
             var sPath = oCtx.getPath();
             //console.log(sPath);
             if (!this.spotEditDialog) {
+                // @ts-ignore
                 this.spotEditDialog = this.loadFragment({
                     name: "sap.ui.demo.webapp.fragment.spotEdit"
                 });
             }
             this.spotEditDialog.then(function (oDialog) {
                 //aPath es global
+                // @ts-ignore
                 oThis.aPath = sPath;
+                // @ts-ignore
                 oThis.setFragmentValues(sPath);
                 oDialog.open();
             });
         },
+        onPressAdd: function () {
+            var oThis = this;
+            if (!this.spotAddDialog) {
+                // @ts-ignore
+                this.spotAddDialog = this.loadFragment({
+                    name: "sap.ui.demo.webapp.fragment.spotAdd"
+                });
+            }
+            this.spotAddDialog.then(function (oDialog) {
+
+                // @ts-ignore
+                var tam = oThis.getOwnerComponent().getModel("spots").getProperty("/SPOT").length;
+                // @ts-ignore
+                oThis.byId("spotAddidID").setValue(tam);
+                // @ts-ignore
+                if (oThis.byId("comboTypes").getItems().length == 0)
+                    // @ts-ignore
+                    oThis.fillTypeComboBox();
+                oDialog.open();
+            });
+        },
         onSaveSpot: function (oEvent) {
-            //console.log("Guardar");
             const oSource = oEvent.getSource();
             const oParent = oSource.getParent();
             const id = oParent.getId();
@@ -68,6 +133,77 @@ sap.ui.define([
             this.getOwnerComponent().getModel("spots").setProperty(this.aPath + "/UBICACION_TECNICA", this.getView().byId("ubitId").getValue());
             this.getOwnerComponent().getModel("spots").setProperty(this.aPath + "/PROVEEDOR", this.getView().byId("provId").getValue());
             this.getOwnerComponent().getModel("spots").setProperty(this.aPath + "/TEL", this.getView().byId("telId").getValue());
+        },
+        onAddSpot: function (oEvent) {
+            console.log("Spot agregado");
+            const oSource = oEvent.getSource();
+            const oParent = oSource.getParent();
+            const id = oParent.getId();
+
+            var tam = this.getOwnerComponent().getModel("spots").getProperty("/SPOT").length;
+            console.log(this.getOwnerComponent().getModel("spots").getProperty("/SPOT"));
+            console.log(tam);
+
+            var type;
+            switch (this._getMasterModel("/newSpot/typeKey")) {
+                case "0":
+                    type = "Success"
+                    break;
+                case "1":
+                    type = "Default"
+                    break;
+                case "2":
+                    type = "Warning"
+                    break;
+                case "3":
+                    type = "None"
+                    break;
+                case "4":
+                    type = "Inactive"
+                    break;
+                case "5":
+                    type = "Error"
+                    break;
+                default:
+                    break;
+            }
+
+            var newItem = {
+                "ID": tam.toString(),
+                "POS": this._getMasterModel("/newSpot/ubicacion"),
+                "TYPE": type,
+                "TOOLTIP": this._getMasterModel("/newSpot/tooltip"),
+                "TEXT": this._getMasterModel("/newSpot/texto"),
+                "ICON": "sap-icon://factory",
+                "ALIGNMENT": "1",
+                "CONTENTOFFSET": "0;80",
+                "IMAGEPATH": [{
+                        "PATH": "/images/plant1_1.png"
+                    },
+                    {
+                        "PATH": "/images/plant1_2.jpg"
+                    }
+                ],
+                "TEL": this._getMasterModel("/newSpot/tel"),
+                "IMAGE_R": "facto",
+                "SCALE": "0.2;0.2",
+
+                "NOMBRE": this._getMasterModel("/newSpot/nombre"),
+                "UBICACION_TECNICA": this._getMasterModel("/newSpot/ubi_tecnica"),
+                "ALERTA": "",
+                "GASTO": Number((Math.random() * (200 - 0) + 0).toFixed(0)),
+                "COLOR_GASTO": "Error",
+                "PROVEEDOR": this._getMasterModel("/newSpot/proveedor"),
+                "CALIDAD": (Math.random() * (99 - 79) + 79).toFixed(2).toString(),
+                "DISPONIBILIDAD": (Math.random() * (95 - 65) + 65).toFixed(2).toString(),
+                "DESEMPENO": (Math.random() * (92 - 59) + 59).toFixed(2).toString(),
+                "OEE": (Math.random() * (80 - 49) + 49).toFixed(2).toString(),
+                "ALERTA_VISIBLE": "0"
+            };
+            this.getOwnerComponent().getModel("spots").setProperty("/SPOT/" + tam + "/", newItem);
+            console.log(this.getOwnerComponent().getModel("spots").getProperty("/SPOT"));
+
+            this.byId(id).close();
         }
     });
 });
