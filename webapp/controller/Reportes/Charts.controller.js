a5b14467b2acb373531733c2dd096c95104ae888
Daniel Castaneda <dcastaneda@baycoconsulting.com>
2021-12-23 11:16:53 -0600

    added new chart view

---
+++ webapp/controller/Reportes/Charts.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+    "sap/ui/demo/webapp/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/integration/widgets/Card"
+], function (Basecontroller, JSONModel, Card) {
+    return Basecontroller.extend("sap.ui.demo.webapp.controller.Reportes.Charts", {
+        onInit: function () {
+            this.getRouter().getRoute("chartView").attachMatched(this._onRouteMatched, this);
+        },
+        _onRouteMatched: function () {
+            const oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/webapp/model/data/cardManifests.json"));
+            this.getView().setModel(oModel, "manifest");
+        },
+
+        lineChartCard: function () {
+            var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/webapp/model/data/cardManifests.json")),
+                oChart = new Card("", {
+                    width:"20rem",
+                    manifest:"{/testa}"
+                });
+            
+            
+
+            oChart.setModel(oModel);
+            this.byId("dynamicFlex").addItem(oChart);
+        }
+    });
+});
